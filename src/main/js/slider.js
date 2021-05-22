6839f13a27f8244cb237eecfb0c18b032bb3b9dc
Admin <example@gmail.com>
2021-05-22 20:22:19 +0300

    feat: added slider to main page

--- src/main/js/slider.js
+++ src/main/js/slider.js
@@ -1,80 +1,56 @@
-/* eslint-disable arrow-body-style */
-const cardsContainer = document.querySelector('.cards');
-const slideLeft = document.querySelector('.slide-left');
-const slideRight = document.querySelector('.slide-right');
+function slide() {
+  const items = document.querySelector('.slider__visible-area');
+  const prev = document.querySelector('.slide-left');
+  const next = document.querySelector('.slide-right');
+  let posInitial;
+  const slides = items.getElementsByClassName('cards');
+  const slidesLength = slides.length;
+  const slideSize = items.getElementsByClassName('cards')[0].offsetWidth;
+  const firstSlide = slides[0];
+  const lastSlide = slides[slidesLength - 1];
+  const cloneFirst = firstSlide.cloneNode(true);
+  const cloneLast = lastSlide.cloneNode(true);
+  let index = 0;
+  let allowShift = true;
 
-let pets;
-let offset = -1080;
+  items.appendChild(cloneFirst);
+  items.insertBefore(cloneLast, firstSlide);
 
-slideLeft.addEventListener('click', () => {
-  offset += -1080;
-  const cards = document.querySelectorAll('.card');
-  for (let i = 0; i < cards.length; i += 1) {
-    cards[i].style.transform = `translateX(${offset}px)`;
+  function shiftSlide(dir, action) {
+    items.classList.add('shifting');
+
+    if (allowShift) {
+      if (!action) { posInitial = items.offsetLeft; }
+      if (dir === 1) {
+        items.style.left = `${posInitial - slideSize}px`;
+        index += 1;
+      } else if (dir === -1) {
+        items.style.left = `${posInitial + slideSize}px`;
+        index -= 1;
+      }
+    }
+
+    allowShift = false;
   }
-});
 
-slideRight.addEventListener('click', () => {
-  offset += 1080;
-  const cards = document.querySelectorAll('.card');
-  for (let i = 0; i < cards.length; i += 1) {
-    cards[i].style.transform = `translateX(${offset}px)`;
+  function checkIndex() {
+    items.classList.remove('shifting');
+    if (index === -1) {
+      items.style.left = `${-(slidesLength * slideSize)}px`;
+      index = slidesLength - 1;
+    }
+
+    if (index === slidesLength) {
+      items.style.left = `${-(1 * slideSize)}px`;
+      index = 0;
+    }
+
+    allowShift = true;
   }
-});
 
-class Card {
-  constructor(name, image) {
-    this.name = name;
-    this.image = image;
-  }
-
-  render() {
-    const card = `<div class="card">
-      <div class="card__image">
-        <img src="${this.image}" alt="Pets">
-      </div>
-      <div class="card__name">${this.name}</div>
-      <button class="card_button" data-name="${this.name}">Learn more</button>
-    </div>`;
-
-    return card;
-  }
+  prev.addEventListener('click', () => shiftSlide(-1));
+  next.addEventListener('click', () => shiftSlide(1));
+  items.addEventListener('transitionend', checkIndex);
 }
 
-const renderCards = () => {
-  pets.sort(() => Math.random() - 0.5);
-
-  for (let i = 0; i < 3; i += 1) {
-    const card = new Card(pets[i].name, pets[i].img);
-    cardsContainer.innerHTML += card.render();
-  }
-
-  const cards = document.querySelectorAll('.card_button');
-  cards.forEach((card) => {
-    card.addEventListener('click', () => {
-      renderModalWindow(card.getAttribute('data-name'));
-    });
-  });
-};
-
-// const createCard = (image, name) => {
-//   return `<div class="card">
-//   <div class="card__image">
-//     <img src="${image}" alt="Pets">
-//   </div>
-//   <div class="card__name">${name}</div>
-//   <button class="card_button" data-value="${name}">Learn more</button>
-// </div>`;
-// };
-
-// const createCards = (cards) => {
-//   for (let i = 0; i < 3; i += 1) {
-//     cardsContainer.innerHTML += createCard(cards[i].img, cards[i].name);
-//   }
-// };
-
-fetch('./assets/json/pets.json')
-  .then((result) => result.json())
-  .then((result) => {
-    pets = result;
-  });
+slide();
